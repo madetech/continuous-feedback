7a95227fa0d6da218d748d53ceba6ea040c30b29
Emile <emile.swarts123@gmail.com>
2016-04-15 11:10:55 +0100

    uh

--- check.rb
+++ check.rb
@@ -1,41 +1,44 @@
-require 'date'
+class BadOnes
+  def initialize
+    @file_contents = File.open('README.md')
+  end
 
-class NaughtyList
-  attr_reader :file, :days
-  def initialize(file, days)
-    @file = file
-    @days = days
+  def blame!
+    offenders = employees.select { |employee| late_for_review?(employee) }
 
-    report(naughty_people)
+    break_build_with!(offenders) unless offenders.empty?
   end
 
   private
 
-  def report(naughty_people)
-    if naughty_people.count > 0
-      puts 'Some people have not had a 1-2-1 recently...'
-      puts naughty_people
-      exit 1
-    end
+  def break_build_with!(offenders)
+    puts offenders
+    abort('Some people have not had a 1-2-1 recently...')
   end
 
-  def naughty_people
-    people.keep_if { |person| naughty?(*person) }
+  def late_for_review?(employee)
+    should_have_had_session_by > last_session_date(employee).to_i
   end
 
-  def people
-    File.open(file).map { |line| name_and_last_date(line) }.compact
+  def last_session_date(employee)
+    Time.new(*last_session(employee))
   end
 
-  def naughty?(name, last_date)
-    last_date + days < Date.today
+  def should_have_had_session_by
+    Time.now.to_i - (60 * 60 * 24 * 14)
   end
 
-  def name_and_last_date(line)
-    name, last_date = line.split('|').map(&:strip).delete_if(&:empty?).compact
-    [name, Date.strptime(last_date, '%d/%m/%Y')]
-  rescue
+  def last_session(employee)
+    matches(employee)[2].split('/').map(&:to_i).reverse
+  end
+
+  def matches(employee)
+    /\| (\w+)\s+\| ([0-9]{2}\/[0-9]{2}\/[0-9]{4})   \|/.match(employee)
+  end
+
+  def employees
+    @file_contents.grep(/[0-9]{2}   \| /)
   end
 end
 
-NaughtyList.new('README.md', 14)
+BadOnes.new.blame!
